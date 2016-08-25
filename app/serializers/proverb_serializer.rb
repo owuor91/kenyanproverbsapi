4caae06330bb45e7fd484d92de388f0df9298c61
John Owuor <owuor91@gmail.com>
2016-08-25 18:49:10 +0300

    setup proverbs serializer

---
+++ app/serializers/proverb_serializer.rb
@@ -0,0 +1,3 @@
+class ProverbSerializer < ActiveModel::Serializer
+  attributes :id
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,6 +10,12 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 0) do
+ActiveRecord::Schema.define(version: 20160825153147) do
+
+  create_table "proverbs", force: :cascade do |t|
+    t.string   "text"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
 end
