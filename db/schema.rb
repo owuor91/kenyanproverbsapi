21e75003db26430cce6ebffb95011e7801ab3c81
John Owuor <owuor91@gmail.com>
2016-08-27 13:36:33 +0300

    migrate to postgresql from sqlite

--- db/schema.rb
+++ db/schema.rb
@@ -12,6 +12,9 @@
 
 ActiveRecord::Schema.define(version: 20160825153147) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "proverbs", force: :cascade do |t|
     t.string   "text"
     t.datetime "created_at", null: false
